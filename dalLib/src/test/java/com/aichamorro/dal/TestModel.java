cb7b23de3eee9f258ac15a7701a9feb01ad5d5c3
Alberto Chamorro <aichamorro.dev@gmail.com>
2014-03-13 19:02:27 +0100

    Added TestModel.java

---
+++ dalLib/src/test/java/com/aichamorro/dal/TestModel.java
@@ -0,0 +1,32 @@
+package com.aichamorro.dal;
+
+import com.aichamorro.dal.dataquery.annotations.ModelField;
+import com.aichamorro.dal.dataquery.annotations.ModelId;
+
+public class TestModel extends ModelImpl {
+	@ModelId
+	private String _id;
+	
+	@ModelField
+	private String _name;
+	
+	public TestModel() {
+		this(null, null);
+	}
+	
+	public TestModel(String id, String name) {
+		_id = id;
+		_name = name;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if( obj instanceof TestModel ) {
+			TestModel compare = (TestModel)obj;
+			
+			return (compare._id.equals(this._id) && compare._name.equals(this._name));
+		}
+		
+		return false;
+	}
+}
